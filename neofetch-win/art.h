42af7f7bad4f5097927d0f89853208f6c1d67216
Catalyzzt <78272915+Catalyzzt@users.noreply.github.com>
2023-10-24 21:31:43 -0400

    Update art.h

--- neofetch-win/art.h
+++ neofetch-win/art.h
@@ -84,36 +84,36 @@ const std::wstring win11art19 = L"llllllllllllllll   llllllllllllllll";
 //llllllllllllllll   llllllllllllllll
 //llllllllllllllll   llllllllllllllll
 
-const std::wstring unknownart01 = L"              c1111111111c                         ";
-const std::wstring unknownart02 = L"           c1111111111111111c                      ";
-const std::wstring unknownart03 = L"           1111111111111111111                     ";
-const std::wstring unknownart04 = L"          1111111111111111111c  c                  ";
-const std::wstring unknownart05 = L"          1111111111111111111   111c             c1";
-const std::wstring unknownart06 = L"         c1111111111111111111  c1111111cccccc11111c";
-const std::wstring unknownart07 = L"         1111111111111111111   1111111111111111111 ";
-const std::wstring unknownart08 = L"         1111111111111111111  c1111111111111111111 ";
-const std::wstring unknownart09 = L"        1111111111111111111c  1111111111111111111  ";
-const std::wstring unknownart10 = L"        1111111111111111111   1111111111111111111  ";
-const std::wstring unknownart11 = L"       1111111111111111111c  c111111111111111111c  ";
-const std::wstring unknownart12 = L"       111111c   cc1111111   1111111111111111111   ";
-const std::wstring unknownart13 = L"      c1c             c111  c111111111111111111c   ";
-const std::wstring unknownart14 = L"          cc111111c         1111111111111111111    ";
-const std::wstring unknownart15 = L"      c111111111111111c     1111111111111111111    ";
-const std::wstring unknownart16 = L"     c111111111111111111c    c1111111111111111     ";
-const std::wstring unknownart17 = L"     1111111111111111111        cc1111111cc        ";
-const std::wstring unknownart18 = L"    c1111111111111111111  c1c               c      ";
-const std::wstring unknownart20 = L"    1111111111111111111c  c1111ccc     ccc11c      ";
-const std::wstring unknownart21 = L"   c1111111111111111111   111111111111111111c      ";
-const std::wstring unknownart22 = L"   1111111111111111111   c111111111111111111       ";
-const std::wstring unknownart23 = L"   1111111111111111111   111111111111111111c       ";
-const std::wstring unknownart24 = L"  c111111111111111111c  c111111111111111111        ";
-const std::wstring unknownart25 = L"  1111111111111111111   1111111111111111111        ";
-const std::wstring unknownart26 = L" c111111111111111111c  c111111111111111111c        ";
-const std::wstring unknownart27 = L" 11c           c1111c  c111111111111111111         ";
-const std::wstring unknownart28 = L"                  c1   111111111111111111c         ";
-const std::wstring unknownart29 = L"                      c111111111111111111          ";
-const std::wstring unknownart30 = L"                       cc111111111111111c          ";
-const std::wstring unknownart31 = L"                          ccc11111ccc              ";
+const std::wstring win701 = L"              c1111111111c                         ";
+const std::wstring win702 = L"           c1111111111111111c                      ";
+const std::wstring win703 = L"           1111111111111111111                     ";
+const std::wstring win704 = L"          1111111111111111111c  c                  ";
+const std::wstring win705 = L"          1111111111111111111   111c             c1";
+const std::wstring win706 = L"         c1111111111111111111  c1111111cccccc11111c";
+const std::wstring win707 = L"         1111111111111111111   1111111111111111111 ";
+const std::wstring win708 = L"         1111111111111111111  c1111111111111111111 ";
+const std::wstring win709 = L"        1111111111111111111c  1111111111111111111  ";
+const std::wstring win710 = L"        1111111111111111111   1111111111111111111  ";
+const std::wstring win711 = L"       1111111111111111111c  c111111111111111111c  ";
+const std::wstring win712 = L"       111111c   cc1111111   1111111111111111111   ";
+const std::wstring win713 = L"      c1c             c111  c111111111111111111c   ";
+const std::wstring win714 = L"          cc111111c         1111111111111111111    ";
+const std::wstring win715 = L"      c111111111111111c     1111111111111111111    ";
+const std::wstring win716 = L"     c111111111111111111c    c1111111111111111     ";
+const std::wstring win717 = L"     1111111111111111111        cc1111111cc        ";
+const std::wstring win718 = L"    c1111111111111111111  c1c               c      ";
+const std::wstring win720 = L"    1111111111111111111c  c1111ccc     ccc11c      ";
+const std::wstring win721 = L"   c1111111111111111111   111111111111111111c      ";
+const std::wstring win722 = L"   1111111111111111111   c111111111111111111       ";
+const std::wstring win723 = L"   1111111111111111111   111111111111111111c       ";
+const std::wstring win724 = L"  c111111111111111111c  c111111111111111111        ";
+const std::wstring win725 = L"  1111111111111111111   1111111111111111111        ";
+const std::wstring win726 = L" c111111111111111111c  c111111111111111111c        ";
+const std::wstring win727 = L" 11c           c1111c  c111111111111111111         ";
+const std::wstring win728 = L"                  c1   111111111111111111c         ";
+const std::wstring win729 = L"                      c111111111111111111          ";
+const std::wstring win730 = L"                       cc111111111111111c          ";
+const std::wstring win731 = L"                          ccc11111ccc              ";
 
 //              c1111111111c                         
 //           c1111111111111111c                      
@@ -147,6 +147,7 @@ const std::wstring unknownart31 = L"                          ccc11111ccc       
 //                          ccc11111ccc              
 
 // identical to win11art but you could customize this if you so choose
+
 
 const std::wstring unknownart01 = L"llllllllllllllll   llllllllllllllll";
 const std::wstring unknownart02 = L"llllllllllllllll   llllllllllllllll";
